a9906e16260c524cf67cc6128190cbcd906c28fa
Ivo Georgiev <ivo@strem.io>
2018-11-21 18:05:47 +0200

    all basic routes

--- bin/sentry.js
+++ bin/sentry.js
@@ -3,6 +3,16 @@ const express = require('express')
 const app = express()
 const port = 8005
 
-app.get('/hello', (req, res) => res.send([]))
+// Channel information: public, cachable
+app.get('/channel/:id/status', (req, res) => res.send({}))
+app.get('/channel/:id/tree', (req, res) => res.send({}))
+app.get('/channel/list', (req, res) => res.send([]))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Channel information: requires auth, cachable
+app.get('/channel/:uid/events', (req, res) => res.send([]))
+
+// Submitting events/messages: requires auth
+app.post('/channel/:id/events', (req, res) => res.send([]))
+app.post('/channel/:id/validator-messages', (req, res) => res.send([]))
+
+app.listen(port, () => console.log(`Sentry listening on port ${port}!`))
