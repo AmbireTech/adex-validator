489aededd8cf0dae4a82f857f79a964130306bd7
Ivo Georgiev <ivo@strem.io>
2018-11-22 12:44:26 +0200

    add prep-db

---
+++ test/prep-db/mongo.js
@@ -0,0 +1,3 @@
+db.sessions.insert({ _id: 'x8c9v1b2', uid: 'awesomeTestUser' })
+db.channels.insert({ _id: 'awesomeTestChannel', status: 'active' })
+
---
+++ test/prep-db/prep-db.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mongo adexValidator ./test/prep-db/mongo.js
+
