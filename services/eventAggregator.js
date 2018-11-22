1a8382d888735347b8ab7c6b5689d01d9ed71211
Ivo Georgiev <ivo@strem.io>
2018-11-22 08:38:18 +0200

    eventAggregator: use proper throttle

--- services/eventAggregator.js
+++ services/eventAggregator.js
@@ -1,3 +1,4 @@
+const { throttle } = require('throttle-debounce')
 const db = require('../db')
 
 const AGGREGATION_THROTTLE = 30*1000
@@ -16,36 +17,44 @@ function record(channelId, userId, events) {
 function makeRecorder(channelId) {
 	const newObject = () => { return { channelId, created: new Date(), events: {} } }
 	const eventAggrCol = db.getMongo().collection('eventAggregates')
+
+	// persist
+	//const saveQueue = []
+	const persist = function(toSave) {
+		// @TODO: proper, one-at-a-time queue
+		eventAggrCol.insertOne(toSave)
+		.catch(function(err) {
+			console.error('eventAggregator fatail error', err)
+		})
+	}
+
+	// persist and reset
 	let o = newObject()
+	const persistAndReset = function() {
+		const toSave = o
+		// this has to be flushed immediately cause otherwise we will drop
+		// everything received while we're sending
+		o = newObject()
+		logAggregate(toSave)
+		// to ensure we always persist toSave's, we have a separate queue
+		persist(toSave)
+	}
+	// @TODO: can this be made to be trailing, not leading?
+	const throttledPersistAndReset = throttle(AGGREGATION_THROTTLE, persistAndReset)
+
 	return function(userId, events) {
 		events.forEach(function(ev) {
 			if (!o.events[userId]) o.events[userId] = {}
 			if (!o.events[userId][ev.type]) o.events[userId][ev.type] = 0
 			o.events[userId][ev.type]++
 		})
-		// @TODO: proper throttling here
-		if (Date.now()-o.created.getTime() > AGGREGATION_THROTTLE) {
-			const toSave = o
-
-			// informative
-			logAggregate(toSave)
-
-			// this has to be flushed immediately cause otherwise we will drop
-			// everything received while we're sending
-			o = newObject()
-			// however, this carries the risk of dropping events if this fails
-			// @TODO: make this a queue of all eventAggregates to be saved, will solve the issue
-			eventAggrCol.insertOne(toSave)
-			.catch(function(err) {
-				console.error('eventAggregator fatail error', err)
-			})
-		}
+		throttledPersistAndReset()
 	}
 }
 
 function logAggregate(o) {
 	// @TODO: optional
-	console.log(`Event aggregate produced: ${o.events}`)
+	console.log(`Event aggregate produced, events for ${Object.keys(o.events).length} users`)
 }
 
 module.exports = { record }
