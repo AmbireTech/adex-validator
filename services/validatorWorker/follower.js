008c3a3b0ac87bbf1c9c5df54f062ce1a2be067e
Ivo Georgiev <ivo@strem.io>
2018-11-24 20:10:54 +0200

    validatorWorker: follower tick: get latest approved state

--- services/validatorWorker/follower.js
+++ services/validatorWorker/follower.js
@@ -1,28 +1,40 @@
 const db = require('../../db')
+const adapter = require('../../adapter')
 
 function tick({channel, newStateTree, balances}) {
-	// @TODO; get previous approved state
-	return getLatestNewStateMsg(channel)
-	.then(function(msg) {
-		console.log(msg)
+	// @TODO: there's a flaw if we use this in a more-than-two validator setup
+	// we compare only OUR last approved state to the NewState
+	// instead, we must compare from the latest state approved by any >=2/3 validators
+	// otherwise, the leader may perform an invalid state transition from latest approved to a NewState, and trick us into signing it cause we'd be comparing with our own (presumably old)
+	return Promise.all([
+		getLatestMsg(channel.id, channel.validators[0], 'NewState'),
+		getLatestMsg(channel.Id, adapter.whoami(), 'ApproveState'),
+	])
+	.then(function([newStateMsg, approveStateMsg]) {
+		if (!newStateMsg) {
+			// there's nothing that we have to do
+			return
+		}
+		// @TODO: compare if newStateMsg is a valid state transition from approveStateMsg
+		console.log(newStateMsg, approveStateMsg)
 	})
 }
 
-function getLatestNewStateMsg(channel) {
+function getLatestMsg(channelId, from, type) {
 	const validatorMsgCol = db.getMongo().collection('validatorMessages')
 	// @TODO: this assumption of getting the latest is flawed; won't work if it's within the same second: https://docs.mongodb.com/manual/reference/method/ObjectId/
 	// it is very important that we get this right, since it will be used to gather data about the channel state too
 	return validatorMsgCol.find({
-		from: channel.validators[0],
-		channelId: channel.id,
-		'msg.type': 'NewState',
+		channelId,
+		from: from,
+		'msg.type': type,
 	})
 	.sort({ _id: -1 })
 	.limit(1)
 	.toArray()
-	.then(function([newStateMsg]) {
+	.then(function([msg]) {
 		// @TODO assert validity
-		return newStateMsg
+		return msg
 	})
 }
 
