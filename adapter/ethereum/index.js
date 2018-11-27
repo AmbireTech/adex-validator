e85b697935d06caa201d3981712a674f27acebd6
Ivo Georgiev <ivo@strem.io>
2018-11-27 23:58:40 +0200

    adapter/ethereum: explain how authentication would work

--- adapter/ethereum/index.js
+++ adapter/ethereum/index.js
@@ -23,10 +23,16 @@ function getBalanceLeaf(acc, bal) {
 function sessionFromToken(token) {
 	console.log('sessionFromToken TODO', token)
 	// @TODO
+	// this will go two ways: either we will have it in the DB and just confirm, or we will not and we will do a full evaluation
 	return Promise.resolve(null)
 }
 function getAuthFor(validator) {
 	// @TODO
+	// we will self-generate a challenge to contain whoever we're authenticating to, the validity period and the current time
+	// we will sign that challenge and use that, and build a complete token containing hash.whoami.challenge.sig
+	// we would allow /session_revoke, which forever revokes the session (early; otherwise it will self-revoke when the validity period expires)
+	// EWT/JWT is kind of similar to this, so reconsider it
+	// also, we need to cache those! and maybe check them first before saving to the DB
 	return Promise.resolve(`AUTH_${whoami()}`)
 }
 
