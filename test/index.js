9f59dca7e5a01bb1c0ddb85857f962fb3b678270
Ivo Georgiev <ivo@strem.io>
2018-12-12 14:05:32 +0000

    tests: detailed tests of isValidTransition

--- test/index.js
+++ test/index.js
@@ -11,23 +11,31 @@ tape('isValidTransition: empty to empty', function(t) {
 })
 
 tape('isValidTransition: a valid transition', function(t) {
-	t.ok(isValidTransition(channel, {}, { a: new BN(9) }), 'is valid transition')
+	t.ok(isValidTransition(channel, {}, { a: new BN(100) }), 'is valid transition')
 	t.end()
 })
 
 tape('isValidTransition: more funds than channel', function(t) {
-	t.notOk(isValidTransition(channel, {}, { a: new BN(101) }), 'not a valid transition')
+	t.notOk(isValidTransition(channel, {}, { a: new BN(51), b: new BN(50) }), 'not a valid transition')
 	t.end()
 })
 
 tape('isValidTransition: single value is lower', function(t) {
+	t.notOk(isValidTransition(channel, { a: new BN(55) }, { a: new BN(54) }), 'not a valid transition')
 	t.end()
 })
 
 tape('isValidTransition: a value is lower, but overall sum is higher', function(t) {
+	t.notOk(isValidTransition(channel, { a: new BN(55) }, { a: new BN(54), b: new BN(3) }), 'not a valid transition')
 	t.end()
 })
 
 tape('isValidTransition: overall sum is lower', function(t) {
+	t.notOk(isValidTransition(channel, { a: new BN(54), b: new BN(3) }, { a: new BN(54) }), 'not a valid transition')
+	t.end()
+})
+
+tape('isValidTransition: overall sum is the same, but we remove an entry', function(t) {
+	t.notOk(isValidTransition(channel, { a: new BN(54), b: new BN(3) }, { a: new BN(57) }), 'not a valid transition')
 	t.end()
 })
