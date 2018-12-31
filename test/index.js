015c6039c20d6f38b9e2e9fe20ea08005265b1ce
Ivo Georgiev <ivo@strem.io>
2018-12-31 18:53:49 +0200

    run-integration-tests: use already set ports, cleanup logic

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const tape = require('tape')
 
 const BN = require('bn.js')
--- test/run-integration-tests.sh
+++ test/run-integration-tests.sh
@@ -4,15 +4,17 @@ TIMESTAMP=`date +%s`
 
 # awesomeLeader, awesomeFollower and all the channels are seeded in the prep-db
 
-LEAD_PORT=8201
+LEAD_PORT=8005
 LEAD_MONGO="testValStackLeader${TIMESTAMP}"
 LEAD_ARGS="--adapter=dummy --dummyIdentity=awesomeLeader"
 
-FOLLOW_PORT=8202
+FOLLOW_PORT=8006
 FOLLOW_MONGO="testValStackFollower${TIMESTAMP}"
 FOLLOW_ARGS="--adapter=dummy --dummyIdentity=awesomeFollower"
 
+# Seeding the database
 echo "Using MongoDB database names: $LEAD_MONGO, $FOLLOW_MONGO"
+# @TODO seed database
 
 # @TODO separate logs
 # Start sentries
@@ -25,9 +27,20 @@ sleep 3
 DB_MONGO_NAME=$LEAD_MONGO bin/validatorWorker.js $LEAD_ARGS &
 DB_MONGO_NAME=$FOLLOW_MONGO bin/validatorWorker.js $FOLLOW_ARGS &
 
-echo "running both"
-sleep 10
+# Run the integration tests
+./test/integration.js
+exitCode=$?
 
+# end all jobs (sentries, workers)
 pkill -P $$
+
+if [ $exitCode -eq 0 ]; then
+	echo "must cleanup DB"
+	# @TODO
+else
+	echo -e "\033[0;31mTests failed, not cleaning up DB\033[0m"
+	echo "MongoDB database names: $LEAD_MONGO, $FOLLOW_MONGO"
+fi
+
 
 # @TODO: cleanup mongo, but only on success
