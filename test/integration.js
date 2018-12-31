19913fb520dabf2e9177d289aab985cd1d01493e
Ivo Georgiev <ivo@strem.io>
2018-12-31 19:42:28 +0200

    test/integration: some basics

--- test/integration.js
+++ test/integration.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
 const tape = require('tape')
+const fetch = require('node-fetch')
 
-tape('connects to sentry', function(t) {
-	t.end()
+// @TODO read this externally?
+const leaderPort = 8005
+const followerPort = 8006
+
+tape('/channel/list', function(t) {
+	fetch(`http://localhost:${leaderPort}/channel/list`)
+	.then(res => res.json())
+	.then(function(resp) {
+		t.ok(Array.isArray(resp.channels), 'resp.channels is an array')
+		t.equal(resp.channels.length, 1, 'resp.channels is the right len')
+		t.equal(resp.channels[0].status, 'live', 'channel is the right status')
+		t.end()
+	})
+	// @TODO: test channel list filters if there are any
 })
 
 // @TODO can't trick with negative values
