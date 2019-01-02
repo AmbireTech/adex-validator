331849f8268373248c2927bae72fdd0f959d844e
Ivo Georgiev <ivo@strem.io>
2019-01-02 20:27:13 +0200

    services/validatorWorker/follower: more comments, changed variable names for more raadability

--- services/validatorWorker/follower.js
+++ services/validatorWorker/follower.js
@@ -14,9 +14,9 @@ function tick(adapter, channel) {
 			.then(augmentWithBalances),
 	])
 	.then(function([newMsg, approveMsg]) {
-		const latestApproved = newMsg && approveMsg && newMsg.stateRoot == approveMsg.stateRoot 
-		// there are no NewState messages, only merge all eventAggrs
-		if (!newMsg || latestApproved) {
+		const latestIsApproved = newMsg && approveMsg && newMsg.stateRoot == approveMsg.stateRoot
+		// there are no unapproved NewState messages, only merge all eventAggrs
+		if (!newMsg || latestIsApproved) {
 			return producer.tick(channel)
 			.then(function(res) {
 				return { nothingNew: !res.newStateTree }
@@ -38,7 +38,11 @@ function onNewState(adapter, {channel, balances, newMsg, approveMsg}) {
 		return { nothingNew: true }
 	}
 
-	const otherValidators = channel.spec.validators.filter(v => v.id != adapter.whoami())
+	// @TODO we should verify the signature of newMsg
+	// currently, in order to POST to /validator-messages, we require that you're a validator
+	// but this means that if a validator's auth token leaks, that can be used to trick followers into signing any new state that is a valid state transition
+	const whoami = adapter.whoami()
+	const otherValidators = channel.spec.validators.filter(v => v.id != whoami)
 	const stateRoot = newMsg.stateRoot
 	const stateRootRaw = Buffer.from(stateRoot, 'hex')
 	return adapter.sign(stateRootRaw)
@@ -59,6 +63,8 @@ function toBNMap(raw) {
 	return balances
 }
 
+// @TODO getLatestMsg should be a part of a DB abstraction so we can use it in other places too
+// e.g. validating on POST /validator-messages (to get the previous), and a public API to get the latest msgs of a type
 function getLatestMsg(channelId, from, type) {
 	const validatorMsgCol = db.getMongo().collection('validatorMessages')
 	// @TODO: this assumption of getting the latest is flawed; won't work if it's within the same second: https://docs.mongodb.com/manual/reference/method/ObjectId/
