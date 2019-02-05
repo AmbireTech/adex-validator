470367abb4fa4c3b838a344c64248d4f9e5b52b4
Omidiora Samuel <8148384+samparsky@users.noreply.github.com>
2019-02-05 12:09:44 +0100

    modify: timestamp

--- services/validatorWorker/heartbeat.js
+++ services/validatorWorker/heartbeat.js
@@ -7,7 +7,7 @@ function heartbeat(adapter, channel){
 	assert.ok(validatorIdx !== -1, 'validatorTick: sending heartbeat for a channel where we are not validating')
 	const otherValidators = channel.spec.validators.filter(v => v.id != whoami)
 
-	const timestamp = new Date().toUTCString()
+	const timestamp = new Date().getTime() / 1000
 	return adapter.sign(timestamp)
 	.then(function(signature) {
 		return persistAndPropagate(adapter, otherValidators, channel, {
