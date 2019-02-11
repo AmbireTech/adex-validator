daa3c8126a4311653d342316f9ff04fed8ca39b8
Omidiora Samuel <8148384+samparsky@users.noreply.github.com>
2019-02-11 16:12:43 +0100

    fix: mergeimpressionev

--- services/sentry/lib/eventReducer.js
+++ services/sentry/lib/eventReducer.js
@@ -1,22 +1,26 @@
+const BN = require('bn.js')
+
 function newAggr(channelId) {
 	return { channelId, created: new Date(), events: {} }
 }
 
-function reduce(userId, aggr, ev) {
+function reduce(userId, aggr, ev, channel) {
 	// for now, we don't use userId
 	// @TODO: this is one of the places to add other ev types
 	if (ev.type === 'IMPRESSION') {
-		aggr.events.IMPRESSION = mergeImpressionEv(aggr.events.IMPRESSION, ev)
+		aggr.events.IMPRESSION = mergeImpressionEv(aggr.events.IMPRESSION, ev, channel)
 	}
 
 	return aggr
 }
 
-function mergeImpressionEv(map, ev) {
+function mergeImpressionEv(map, ev, channel) {
 	if (typeof(ev.publisher)!=='string') return map
-	if (!map) map = {}
-	if (!map[ev.publisher]) map[ev.publisher] = 0
-	map[ev.publisher]++
+	if (!map) map = { eventCounts: {}, eventPayouts: {} }
+	if (!map.eventCounts[ev.publisher]) map.eventCounts[ev.publisher] = 0
+	if (!map.eventPayouts[ev.publisher]) map.eventPayouts[ev.publisher] = new BN(0)
+	map.eventCounts[ev.publisher]++
+	map.eventPayouts[ev.publisher].add(new BN(channel.minPerImpression || 1, 10))
 	return map
 }
 
