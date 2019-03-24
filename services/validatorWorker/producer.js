a29f770c35a677ad07100987f61364c1345ba599
Ivo Georgiev <ivo@strem.io>
2019-03-24 23:18:34 +0200

    validatorWorker/producer: slight re-work

--- services/validatorWorker/producer.js
+++ services/validatorWorker/producer.js
@@ -11,11 +11,10 @@ async function tick(iface, channel) {
 	// Process eventAggregates, from old to new, newer than the lastEvAggr time
 	const aggrs = await iface.getEventAggrs({ after: accounting.lastEvAggr })
 
-	logMerge(channel, aggrs)
-
 	// mergeAggr will add all eventPayouts from aggrs to the balancesBeforeFees
 	// and produce a new accounting message
 	if (aggrs.length) {
+		logMerge(aggrs, channel)
 		const { balances, newAccounting } = mergeAggrs(accounting, aggrs, channel)
 		await iface.propagate([newAccounting])
 		return { balances, newAccounting }
@@ -23,12 +22,9 @@ async function tick(iface, channel) {
 	return { balances: toBNMap(accounting.balances) }
 }
 
-function logMerge(channel, eventAggrs) {
-	if (eventAggrs.length === 0) return
+function logMerge(aggrs, channel) {
 	// eslint-disable-next-line no-console
-	console.log(
-		`validatorWorker: channel ${channel.id}: processing ${eventAggrs.length} event aggregates`
-	)
+	console.log(`validatorWorker: channel ${channel.id}: processing ${aggrs.length} event aggregates`)
 }
 
 module.exports = { tick }
