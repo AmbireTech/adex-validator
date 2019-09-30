08813fd5964ad573241bedef9d57335e27d4acf6
Ivo Georgiev <ivo@strem.io>
2019-09-30 08:30:18 +0300

    schemas: fix allowed timeframes

--- routes/schemas.js
+++ routes/schemas.js
@@ -160,7 +160,7 @@ module.exports = {
 			.valid(['eventCounts', 'eventPayouts'])
 			.default('eventCounts'),
 		timeframe: Joi.string()
-			.valid(['month', 'week', 'day', 'minute', 'hour'])
+			.valid(['year', 'month', 'week', 'day', 'hour'])
 			.default('hour'),
 		limit: Joi.number().default(100)
 	}
