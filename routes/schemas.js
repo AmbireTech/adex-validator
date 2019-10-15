dfab27dee3f923a451fac078246533a5629d4b4a
Ivo Georgiev <ivo@strem.io>
2019-10-15 10:39:03 +0300

    Revert "try to default the fee to 0"
    This reverts commit 55f9710f230cdd222f33960991779988a1de0672.

--- routes/schemas.js
+++ routes/schemas.js
@@ -93,7 +93,7 @@ module.exports = {
 								scheme: ['http', 'https']
 							})
 							.required(),
-						fee: numericString.default('0').required()
+						fee: numericString.required()
 					})
 				)
 				.required()
