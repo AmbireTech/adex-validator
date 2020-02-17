e7f525a207769e8574ef89a2b016713a00eae4e9
Ivo Georgiev <ivo@strem.io>
2020-02-17 23:41:51 +0200

    distribute-rewards: calculate all datapoints

--- scripts/distribute-rewards.js
+++ scripts/distribute-rewards.js
@@ -2,13 +2,20 @@
 const ethers = require('ethers')
 
 const { Contract, getDefaultProvider } = ethers
+const BN = require('bn.js')
+const fetch = require('node-fetch')
 const STAKING_ABI = require('adex-protocol-eth/abi/Staking')
-
 const cfg = require('../cfg')
 
 // Staking started on 28-12-2019
 const STAKING_START_MONTH = new Date('01-01-2020')
 const ADDR_STAKING = '0x46ad2d37ceaee1e82b70b867e674b903a4b4ca32'
+
+// const POOL_ID = '0x2ce0c96383fb229d9776f33846e983a956a7d95844fac57b180ed0071d93bb28'
+const POOL_VALIDATOR_URL = 'https://tom.adex.network'
+
+const REWARD_NUM = new BN(5)
+const REWARD_DEN = new BN(100)
 
 const provider = getDefaultProvider('homestead')
 const Staking = new Contract(ADDR_STAKING, STAKING_ABI, provider)
@@ -24,21 +31,42 @@ const Staking = new Contract(ADDR_STAKING, STAKING_ABI, provider)
 // cfg relayer would also be useful
 console.log(cfg.ETHEREUM_ADAPTER_RELAYER)
 
-// Produce all intervals: monthly
-const getNextMonth = n =>
-	n.getMonth() === 11
-		? new Date(n.getFullYear() + 1, 0, 1)
-		: new Date(n.getFullYear(), n.getMonth() + 1, 1)
-const now = new Date()
-let start = new Date(STAKING_START_MONTH)
-const intervals = []
-while (true) {
-	// Current month is not over, so it's not included
-	if (start.getFullYear() === now.getFullYear() && start.getMonth() === now.getMonth()) break
-	const next = getNextMonth(start)
-	intervals.push({ start, end: next })
-	start = next
+console.log(Staking)
+
+getIntervalsWithRewards(STAKING_START_MONTH).then(console.log)
+
+function getIntervals(startDate) {
+	let start = new Date(startDate)
+	// Produce all intervals: monthly
+	const getNextMonth = n =>
+		n.getMonth() === 11
+			? new Date(n.getFullYear() + 1, 0, 1)
+			: new Date(n.getFullYear(), n.getMonth() + 1, 1)
+	const now = new Date()
+	const intervals = []
+	while (true) {
+		// Current month is not over, so it's not included
+		if (start.getFullYear() === now.getFullYear() && start.getMonth() === now.getMonth()) break
+		const next = getNextMonth(start)
+		intervals.push({ start, end: next })
+		start = next
+	}
+
+	return intervals
 }
 
-console.log(intervals)
-console.log(Staking)
+async function getIntervalsWithRewards(startDate) {
+	const intervals = getIntervals(startDate)
+	return Promise.all(
+		intervals.map(async ({ start, end }) => {
+			const url = `${POOL_VALIDATOR_URL}/analytics?timeframe=day&metric=eventPayouts&start=${start}&end=${end}`
+			const resp = await fetch(url).then(r => r.json())
+			console.log(resp)
+			const toDistribute = resp.aggr.map(({ value, time }) => ({
+				time,
+				value: new BN(value).mul(REWARD_NUM).div(REWARD_DEN)
+			}))
+			return { start, end, toDistribute }
+		})
+	)
+}
