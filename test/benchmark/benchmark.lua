93aa70f56e8243d7f1d5d2af5c92f7b22648490e
Ivo Georgiev <ivo@strem.io>
2020-02-25 00:13:43 +0200

    benchmark: update hardcoded token/publisher

--- test/benchmark/benchmark.lua
+++ test/benchmark/benchmark.lua
@@ -1,5 +1,5 @@
 wrk.method = "POST"
-wrk.body   = "{ \"events\": [ {\"type\": \"IMPRESSION\", \"publisher\": \"awesomePublisher\"} ] }"
+wrk.body   = "{ \"events\": [ {\"type\": \"IMPRESSION\", \"publisher\": \"0xb7d3f81e857692d13e9d63b232a90f4a1793189e\"} ] }"
 wrk.headers["Content-Type"] = "application/json"
 -- uses the creator token
-wrk.headers["authorization"] = "Bearer awesomeCreator"
+wrk.headers["authorization"] = "Bearer 0x033ed90e0fec3f3ea1c9b005c724d704501e0196"
