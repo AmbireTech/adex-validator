aa86c7506f6b3ececa481ab6723e6c875e1c14d2
Ivo Georgiev <ivo@strem.io>
2020-06-02 22:51:30 +0300

    shimTargeting: only shim the ones w/o set targetingRules

--- scripts/shimTargeting.js
+++ scripts/shimTargeting.js
@@ -12,7 +12,7 @@ async function shimTargeting() {
 	await db.connect()
 
 	const channelCol = db.getMongo().collection('channels')
-	const channels = await channelCol.find()
+	const channels = await channelCol.find({ targetingRules: { $exists: false } })
 
 	while (await channels.hasNext()) {
 		const channel = await channels.next()
