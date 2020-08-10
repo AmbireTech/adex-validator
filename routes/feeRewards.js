96454823838d7104de9d9a167b429c2068da539d
Ivo Georgiev <ivo@strem.io>
2020-08-10 14:52:06 +0300

    feeRewards: only return valid channels

--- routes/feeRewards.js
+++ routes/feeRewards.js
@@ -5,7 +5,7 @@ module.exports = async (_, res) =>
 		await db
 			.getMongo()
 			.collection('rewardChannels')
-			.find({ validUntil: { $gt: Math.floor(Date.now() / 1000) } })
+			.find({ 'channelArgs.validUntil': { $gt: Math.floor(Date.now() / 1000) } })
 			.sort({ periodStart: -1 })
 			.limit(40)
 			.toArray()
