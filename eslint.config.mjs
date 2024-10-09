54970817ae93714e3fe56aeac5a9ca1dc9c68060
IvoPaunov <ivo.paunov@gmail.com>
2024-10-09 12:44:08 +0300

    add prettier to eslint cfg

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier"
 
 
 export default [
@@ -9,5 +10,6 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  {ignores: ['node_modules', 'distribute-rewards.js']}
+  {ignores: ['node_modules', 'distribute-rewards.js']},
+  eslintConfigPrettier
 ];
